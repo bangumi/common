8f9e3b53d6c0a455bc9d12758199e0e1bbd97b14
Trim21 <trim21.me@gmail.com>
2024-09-23 02:03:23 +0800

    add sort keys

--- platform.py
+++ platform.py
@@ -1,18 +1,24 @@
 from pathlib import Path
 
 import msgspec
+import yaml
 
 
 class Platform(msgspec.Struct):
     id: int
     type: str
     type_cn: str
-    sortKeys: tuple[str, ...] = ()
+    sort_keys: tuple[str, ...] = ()
 
+
+subject_platforms = yaml.safe_load(
+    Path(__file__, "../subject_platforms.yml").resolve().read_bytes()
+)
 
 PlatformConfig = dict[int, dict[int, Platform]]
+SortKeys = msgspec.convert(subject_platforms['sort_keys'], dict[int, tuple[str, ...]])
 
-
-PLATFORM_CONFIG: PlatformConfig = msgspec.json.decode(
-    Path(__file__, "../platform.json").resolve().read_bytes(), type=PlatformConfig
+PLATFORM_CONFIG: PlatformConfig = msgspec.convert(
+    {key: value for key, value in subject_platforms['platforms'].items() if isinstance(key, int)},
+    type=PlatformConfig
 )
